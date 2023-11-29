7257613bfea2e4b9a6f452b510a2a8c89e539e38
ldphat99 <ldphat99@gmail.com>
2023-11-29 22:38:00 +0700

    temp

--- src/views/ProductDetailView.vue
+++ src/views/ProductDetailView.vue
@@ -25,9 +25,9 @@
                             :content-style="{ color: '#bb0b0b', fontWeight: 'bold' }">${{
                                 product.price }}</a-descriptions-item>
                         <a-descriptions-item label="Quantity" :span="1">{{ product.quantityInStock }}</a-descriptions-item>
-                        <a-descriptions-item label="Description" :span="2" :content-style="{ textAlign: 'justify' }">
+                        <!-- <a-descriptions-item label="Description" :span="2" :content-style="{ textAlign: 'justify' }">
                             <div v-html="product.description"></div>
-                        </a-descriptions-item>
+                        </a-descriptions-item> -->
                         <a-descriptions-item label="Category" :span="2">
                             <a-tag color="cyan">{{ product.category }}</a-tag>
                         </a-descriptions-item>
@@ -42,11 +42,168 @@
                     </a-descriptions>
                 </div>
             </a-col>
+            <a-col :span="24">
+                <div
+                    style="padding: 0.875rem; background: rgba(0,0,0,.02); color: rgba(0,0,0,.87); font-size: 1.25rem; font-weight: 400; text-transform: capitalize;">
+                    DESCRIPTION</div>
+                <div v-if="loading">
+                    <a-skeleton active :paragraph="{ rows: 4 }" />
+                </div>
+                <div v-else style="padding: 0.875rem; margin-top: 16px;">
+                    <div v-html="product.description"></div>
+                </div>
+            </a-col>
+            <a-col :span="24">
+                <div
+                    style="padding: 0.875rem; background: rgba(0,0,0,.02); color: rgba(0,0,0,.87); font-size: 1.25rem; font-weight: 400; text-transform: capitalize;">
+                    USER REVIEWS</div>
+                <a-comment>
+                    <template #actions>
+                        <span key="comment-basic-like">
+                            <a-tooltip title="Like">
+                                <template v-if="action === 'liked'">
+                                    <LikeFilled @click="like" />
+                                </template>
+                                <template v-else>
+                                    <LikeOutlined @click="like" />
+                                </template>
+                            </a-tooltip>
+                            <span style="padding-left: 8px; cursor: auto">
+                                {{ likes }}
+                            </span>
+                        </span>
+                        <span key="comment-basic-dislike">
+                            <a-tooltip title="Dislike">
+                                <template v-if="action === 'disliked'">
+                                    <DislikeFilled @click="dislike" />
+                                </template>
+                                <template v-else>
+                                    <DislikeOutlined @click="dislike" />
+                                </template>
+                            </a-tooltip>
+                            <span style="padding-left: 8px; cursor: auto">
+                                {{ dislikes }}
+                            </span>
+                        </span>
+                        <span key="comment-basic-reply-to">Reply to</span>
+                    </template>
+                    <template #author><a>Han Solo</a></template>
+                    <template #avatar>
+                        <a-avatar src="https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png" alt="Han Solo" />
+                    </template>
+                    <template #content>
+                        <p>
+                            We supply a series of design principles, practical patterns and high quality design
+                            resources (Sketch and Axure), to help people create their product prototypes beautifully and
+                            efficiently.
+                        </p>
+                    </template>
+                    <template #datetime>
+                        <a-tooltip :title="dayjs().format('YYYY-MM-DD HH:mm:ss')">
+                            <span>{{ dayjs().fromNow() }}</span>
+                        </a-tooltip>
+                    </template>
+                </a-comment>
+                <a-comment>
+                    <template #actions>
+                        <span key="comment-basic-like">
+                            <a-tooltip title="Like">
+                                <template v-if="action === 'liked'">
+                                    <LikeFilled @click="like" />
+                                </template>
+                                <template v-else>
+                                    <LikeOutlined @click="like" />
+                                </template>
+                            </a-tooltip>
+                            <span style="padding-left: 8px; cursor: auto">
+                                {{ likes }}
+                            </span>
+                        </span>
+                        <span key="comment-basic-dislike">
+                            <a-tooltip title="Dislike">
+                                <template v-if="action === 'disliked'">
+                                    <DislikeFilled @click="dislike" />
+                                </template>
+                                <template v-else>
+                                    <DislikeOutlined @click="dislike" />
+                                </template>
+                            </a-tooltip>
+                            <span style="padding-left: 8px; cursor: auto">
+                                {{ dislikes }}
+                            </span>
+                        </span>
+                        <span key="comment-basic-reply-to">Reply to</span>
+                    </template>
+                    <template #author><a>Han Solo</a></template>
+                    <template #avatar>
+                        <a-avatar src="https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png" alt="Han Solo" />
+                    </template>
+                    <template #content>
+                        <p>
+                            We supply a series of design principles, practical patterns and high quality design
+                            resources (Sketch and Axure), to help people create their product prototypes beautifully and
+                            efficiently.
+                        </p>
+                    </template>
+                    <template #datetime>
+                        <a-tooltip :title="dayjs().format('YYYY-MM-DD HH:mm:ss')">
+                            <span>{{ dayjs().fromNow() }}</span>
+                        </a-tooltip>
+                    </template>
+                </a-comment>
+                <a-comment>
+                    <template #actions>
+                        <span key="comment-basic-like">
+                            <a-tooltip title="Like">
+                                <template v-if="action === 'liked'">
+                                    <LikeFilled @click="like" />
+                                </template>
+                                <template v-else>
+                                    <LikeOutlined @click="like" />
+                                </template>
+                            </a-tooltip>
+                            <span style="padding-left: 8px; cursor: auto">
+                                {{ likes }}
+                            </span>
+                        </span>
+                        <span key="comment-basic-dislike">
+                            <a-tooltip title="Dislike">
+                                <template v-if="action === 'disliked'">
+                                    <DislikeFilled @click="dislike" />
+                                </template>
+                                <template v-else>
+                                    <DislikeOutlined @click="dislike" />
+                                </template>
+                            </a-tooltip>
+                            <span style="padding-left: 8px; cursor: auto">
+                                {{ dislikes }}
+                            </span>
+                        </span>
+                        <span key="comment-basic-reply-to">Reply to</span>
+                    </template>
+                    <template #author><a>Han Solo</a></template>
+                    <template #avatar>
+                        <a-avatar src="https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png" alt="Han Solo" />
+                    </template>
+                    <template #content>
+                        <p>
+                            We supply a series of design principles, practical patterns and high quality design
+                            resources (Sketch and Axure), to help people create their product prototypes beautifully and
+                            efficiently.
+                        </p>
+                    </template>
+                    <template #datetime>
+                        <a-tooltip :title="dayjs().format('YYYY-MM-DD HH:mm:ss')">
+                            <span>{{ dayjs().fromNow() }}</span>
+                        </a-tooltip>
+                    </template>
+                </a-comment>
+            </a-col>
         </a-row>
     </div>
 </template>
 
-<script setup>
+<script setup >
 import { ref, onMounted, h } from 'vue';
 import axios from 'axios';
 import { useRouter, useRoute } from 'vue-router';
@@ -54,7 +211,28 @@ import { PlusOutlined, ShoppingCartOutlined } from '@ant-design/icons-vue';
 import { useCartStore } from '../store/cartStore';
 import { useAuthStore } from '../store/authStore';
 import { message } from 'ant-design-vue';
+import dayjs from 'dayjs';
+import { LikeFilled, LikeOutlined, DislikeFilled, DislikeOutlined } from '@ant-design/icons-vue';
+import relativeTime from 'dayjs/plugin/relativeTime';
 
+
+dayjs.extend(relativeTime);
+
+const likes = ref(0);
+const dislikes = ref(0);
+const action = ref();
+
+const like = () => {
+  likes.value = 1;
+  dislikes.value = 0;
+  action.value = 'liked';
+};
+
+const dislike = () => {
+  likes.value = 0;
+  dislikes.value = 1;
+  action.value = 'disliked';
+};
 
 
 
@@ -159,7 +337,7 @@ onMounted(async () => {
         if (response.status === 200) {
             product.value = response.data.product;
             loading.value = false;
-            console.log(product.value);
+            // console.log(product.value);
         }
     } catch (error) {
         console.error('Error fetching product details:', error);
